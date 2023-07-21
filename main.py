141f0fddb0c5bc9ad4834719ccffeaae53ab1073
HYF66 <60986268+HYF66@users.noreply.github.com>
2023-07-21 10:09:12 +0800

    Changes

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+from tensorflow.keras.models import load_model
+from training import build_and_train_models
+import tensorflow as tf
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# By saving the weights, it allows you to improve and retrain the model
+"""
+the original model used 2000 steps, here I just load a small number of steps
+to highlight the process, and get the model archetecture to load the weights into..
+..if you want to retrain the model add more steps than 10 and don't load the weights.
+"""
+steps = 10  # original model used 2000 (choose a low number if you)
+gen0, gen1 = build_and_train_models(train_steps=steps)
 
+# Comment and uncomment to load/save models
+# gen1.save("gen0")
+# gen0.save_weights("gen1_weights")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Load the fully developed model.
 
+gen0 = load_model("gen0")
+gen1.load_weights('gen1_weights')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tf.keras.utils.plot_model(gen1, to_file='model.png', show_shapes=False, show_layer_names=True, rankdir='TB',
+                          expand_nested=False, dpi=96)
